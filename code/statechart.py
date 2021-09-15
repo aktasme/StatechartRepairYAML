b3c04f66268056c86fc0780650e976db4f8e8f7f
Mehmet <aktasme@users.noreply.github.com>
2021-09-16 00:21:49 +0300

    File extensions fixed.

--- code/statechart.py
+++ code/statechart.py
@@ -5,20 +5,20 @@ from pathlib import Path
 from commonlog import CommonLog
 
 class Statechart(CommonLog):
-    def __init__(self, sourceFile):
+    def __init__(self, sourceString):
         CommonLog.__init__(self)
-        self.sourceFile = sourceFile
-        self.sorcePlantumlFile = str(sourceFile) + '.plant'
-        self.statechart = sismic.io.import_from_yaml(filepath=sourceFile)
+        self.sourceFile = Path(sourceString)
+        self.sourcePlantumlFile = self.sourceFile.with_suffix('.plant')
+        self.statechart = sismic.io.import_from_yaml(filepath=sourceString)
         assert isinstance(self.statechart, sismic.model.Statechart)
-        self.targetDirectory = Path('results').joinpath(sourceFile.parts[1])
+        self.targetDirectory = Path('results').joinpath(sourceString.parts[1])
         self.targetDirectory.mkdir(parents=True, exist_ok=True)
-        self.targetFile = self.targetDirectory.joinpath(sourceFile.parts[2])
-        self.targetPlantumlFile = str(self.targetFile) + '.plant'
+        self.targetFile = self.targetDirectory.joinpath(sourceString.parts[2])
+        self.targetPlantumlFile = self.targetFile.with_suffix('.plant')
         self.name = self.statechart.name
 
-        sismic.io.export_to_plantuml(self.statechart, self.sorcePlantumlFile)
-        subprocess.call(['java', '-jar', 'plantuml.jar', self.sorcePlantumlFile])
+        sismic.io.export_to_plantuml(self.statechart, self.sourcePlantumlFile)
+        subprocess.call(['java', '-jar', 'plantuml.jar', self.sourcePlantumlFile])
         
         self.hasCrossLevelTransition = False
         self.hasMissingEvent = False
